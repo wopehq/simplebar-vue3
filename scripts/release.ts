d82e542435c79940d6789ef3935bd9571a6e3cec
Kadir Yazıcı <kyzc411@outlook.com.tr>
2021-10-09 15:04:51 +0300

    complete release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -8,6 +8,10 @@ import pkgJSON from '../package.json';
 
 const root = process.cwd();
 
+interface PromptVal<T> {
+   value: T;
+}
+
 async function main() {
    let gitignore = await readGitignore();
    const ignoredList = gitignore.split('\n').filter(Boolean);
@@ -21,26 +25,48 @@ async function main() {
       return { title: v, value: v };
    });
 
-   const { value: newVersion } = await prompt({
+   const { value: newVersion }: PromptVal<string> = await prompt({
       type: 'text',
       name: 'value',
-      message: 'Select new version'
+      message: 'Select new version',
+      initial: pkgJSON.version
    });
 
-   const { value: gitAddFiles } = await prompt({
+   let { value: gitAddFiles }: PromptVal<string[]> = await prompt({
       type: 'multiselect',
       name: 'value',
       message: `Select files to be pushed`,
       choices: [{ title: 'All Files', value: '.' }, ...fileChoices]
    });
+
+   if (gitAddFiles.length > 1 && gitAddFiles.includes('.')) {
+      gitAddFiles = ['.'];
+   }
+
+   const { value: commitMessage }: PromptVal<string> = await prompt({
+      type: 'text',
+      name: 'value',
+      message: 'Git commit message',
+      min: 3
+   });
+
+   const { value: branchName }: PromptVal<string> = await prompt({
+      type: 'text',
+      name: 'value',
+      message: 'branch',
+      initial: 'main'
+   });
+
+   const newPkgJson = { ...pkgJSON, version: newVersion };
+   await writeNewPackageJson(newPkgJson);
+
+   await execute('git', ['add', ...gitAddFiles]);
+   await execute('git', ['commit', '-m', commitMessage]);
+   await execute('git', ['push', 'origin', branchName]);
 }
 main();
 
 async function readGitignore() {
-   return await fse.readFile(join(root, '.gitignore'), 'utf8');
-}
-
-async function readPackageJson() {
    return await fse.readFile(join(root, '.gitignore'), 'utf8');
 }
 
@@ -49,7 +75,7 @@ async function writeNewPackageJson(newPKG: typeof pkgJSON) {
    return await fse.writeFile(join(root, 'package.json'), stringifiedPackageJSON);
 }
 
-async function execute(command: string, commandArguments: string[], options: Options) {
+async function execute(command: string, commandArguments: string[], options?: Options) {
    await execa(command, commandArguments, {
       ...options,
       stdio: 'inherit'
