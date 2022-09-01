63690fcc9defb1fdaf3ab48135a172aacc464b1c
Kadir Yazıcı <47540799+kadiryazici@users.noreply.github.com>
2022-09-01 17:26:11 +0300

    Fix buld & remove release and add workflow (#10)
    * Fix build and remove release
    
    * Add workflow
    
    * Update publish.yml
    
    * Update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import * as fse from 'fs-extra';
 
 const root = process.cwd();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [vue()],
+   plugins: [
+      vue(), //
+      {
+         name: 'plugin:move-types',
+         apply: 'build',
+         async closeBundle() {
+            const fileName = 'lib.d.ts';
+            const libDTS = path.join(root, 'src', fileName);
+            const distLibDTS = path.join(root, 'dist', fileName);
+            await fse.copyFile(libDTS, distLibDTS);
+         }
+      }
+   ],
    build: {
       target: 'es2020',
-
       lib: {
          entry: path.resolve(root, 'src', 'lib.ts'),
          name: 'simplebar-vue3',
